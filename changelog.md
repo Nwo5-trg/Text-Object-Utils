ead239eb1780079048f37a0a1cb3cdcad2f82b12
nwo5-trg <dogamations@gmail.com>
2025-07-20 13:22:26 -0400

    rwarrrrrr

--- changelog.md
+++ changelog.md
@@ -1,5 +1,11 @@
+## v1.0.3
+- **Added** use level font (thx for the pr **ReallyNewb**)
+
+- **Fixed** maybe support with alphas multi line text mod???? idk
+- **Fixed** split text crash
+
 ## v1.0.2
-- **Fixed** better edit compact
+- **Fixed** better edit compact (reported by **Doranell**)
 
 ## v1.0.1
 - **Fixed** node ids
--- src/main.cpp
+++ src/main.cpp
@@ -4,85 +4,57 @@
 
 using namespace geode::prelude;
 
-auto mod = Mod::get();
-
 class $modify(EditTextLayer, CustomizeObjectLayer) {
     struct Fields {
-        TextInput* newTextInput;
-
-        bool swapCopyPaste = mod->getSettingValue<bool>("swap-copy-paste-buttons");
-        std::string lineShortcut = mod->getSettingValue<std::string>("new-line-shortcut");
+        TextGameObject* textObject = nullptr;
+        bool swapCopyPaste = Mod::get()->getSettingValue<bool>("swap-copy-paste-buttons");
+        std::string lineShortcut = Mod::get()->getSettingValue<std::string>("new-line-shortcut");
     };
 
     bool init(GameObject* p0, CCArray* p1) {
         if (!CustomizeObjectLayer::init(p0, p1)) return false;
     
-        auto defaultInput = m_textInput;
         if (!m_textInput) return true;
 
         auto& lineShortcut = m_fields->lineShortcut;
         if (lineShortcut.empty()) lineShortcut = "/n"; // fuck whatever user trys this
 
 
-        auto textObject = static_cast<TextGameObject*>(p0);
-        auto defaultInputBG = static_cast<CCScale9Sprite*>(m_mainLayer->getChildByID("text-input-bg"));
-        defaultInputBG->setOpacity(0);
+        m_fields->textObject = static_cast<TextGameObject*>(p0);
+        m_textInput->setPositionY(m_textInput->getPositionY() - 20);
+        auto inputBG = static_cast<CCScale9Sprite*>(m_mainLayer->getChildByID("text-input-bg"));
+        inputBG->setPositionY(inputBG->getPositionY() - 20);
+        inputBG->setContentSize({inputBG->getContentWidth() - 40, inputBG->getContentHeight()});
         m_kerningLabel->setPositionY(m_kerningLabel->getPositionY() - 20);
         m_kerningSlider->setPositionY(m_kerningSlider->getPositionY() - 20);
-        defaultInput->setPositionY(500.0f); // fuck you im lazy
-        this->getChildByIDRecursive("clear-text-button")->setPositionY(500.0f);
+        this->getChildByIDRecursive("clear-text-button")->setPositionY(500.0f); // why not keep it? idk :3c
         
         if (Loader::get()->isModLoaded("hjfod.betteredit")) this->scheduleOnce(schedule_selector(EditTextLayer::openTextMenu), 0);
-        else openTextMenu(0);
-
-
-        auto input = TextInput::create(200.0f, "");
-        input->setPosition(defaultInputBG->getPositionX(), defaultInputBG->getPositionY() - 20);
-        auto inputPos = input->getPosition();
-        input->setCommonFilter(CommonFilter::Any);
-        input->setString(textObject->m_text);
-        input->setCallback([textObject, input, lineShortcut] (const std::string& inputString) {
-            if (!textObject);
-            else if (inputString.empty()) textObject->updateTextObject("a", false);
-            else {
-                auto str = inputString;
-                int pos = 0;
-                while ((pos = str.find(lineShortcut, pos)) != std::string::npos) {
-                    str.replace(pos, lineShortcut.length(), "\n");
-                    pos += 1;
-                }
-                textObject->updateTextObject(str, false);
-                input->setString(str);
-            }
-        });
-        input->setID("text-object-utils-input");
-        m_mainLayer->addChild(input);
-        m_textTabNodes->addObject(input); // so it hides
-        m_fields->newTextInput = input;
-
+        else openTextMenu(0.0f);
+ 
         auto menu = CCMenu::create();
         menu->setPosition(0.0f, 0.0f);
-        menu->setID("text-object-utils-menu");
+        menu->setID("text-object-utils-menu"_spr);
         m_mainLayer->addChild(menu);
         m_textTabNodes->addObject(menu);
 
         auto copyButton = CCMenuItemSpriteExtra::create(
         CCSprite::createWithSpriteFrameName("GJ_copyBtn_001.png"), this, menu_selector(EditTextLayer::onCopyText));
-        copyButton->setPosition(inputPos.x + (m_fields->swapCopyPaste ? -120 : 120), inputPos.y);
+        copyButton->setPosition(inputBG->getPositionX() + (m_fields->swapCopyPaste ? -120 : 120), inputBG->getPositionY());
         copyButton->setScale(0.5f);
         copyButton->m_baseScale = 0.5f;
         menu->addChild(copyButton);
 
         auto pasteButton = CCMenuItemSpriteExtra::create(
         CCSprite::createWithSpriteFrameName("GJ_pasteBtn_001.png"), this, menu_selector(EditTextLayer::onPasteText));
-        pasteButton->setPosition(inputPos.x + (m_fields->swapCopyPaste ? 120 : -120), inputPos.y);
+        pasteButton->setPosition(inputBG->getPositionX() + (m_fields->swapCopyPaste ? 120 : -120), inputBG->getPositionY());
         pasteButton->setScale(0.5f);
         pasteButton->m_baseScale = 0.5f;
         menu->addChild(pasteButton);
 
         auto clearButton = CCMenuItemSpriteExtra::create(
         CCSprite::createWithSpriteFrameName("GJ_trashBtn_001.png"), this, menu_selector(EditTextLayer::onClearText));
-        clearButton->setPosition(inputPos.x + 150, inputPos.y);
+        clearButton->setPosition(inputBG->getPositionX() + 150, inputBG->getPositionY());
         clearButton->setScale(0.5f);
         clearButton->m_baseScale = 0.5f;
         menu->addChild(clearButton);
@@ -90,27 +62,29 @@ class $modify(EditTextLayer, CustomizeObjectLayer) {
         return true;
     }
 
-    void onClose(CCObject* sender) {
-        if (auto input = m_fields->newTextInput) input->getInputNode()->onClickTrackNode(false); // text inputs are unstable fucks that crash if deleted while selected
-        CustomizeObjectLayer::onClose(sender);
+    void textChanged(CCTextInputNode* p0) {
+        auto fields = m_fields.self();
+        auto str = p0->getString();
+        // p sure geode has a util for this that ive literally used before but its fiiiiiiiine
+        int pos = 0;
+        while ((pos = str.find(fields->lineShortcut, pos)) != std::string::npos) {
+            str.replace(pos, fields->lineShortcut.length(), "\n");
+            pos += 1;
+        }
+        p0->setString(str);
+        CustomizeObjectLayer::textChanged(p0);
     }
 
     void onCopyText(CCObject* sender) {
-        auto input = m_fields->newTextInput;
-        if (!input) return;
-        clipboard::write(input->getString());
+        if (m_textInput) clipboard::write(m_textInput->getString());
     }
 
     void onPasteText(CCObject* sender) {
-        auto input = m_fields->newTextInput;
-        if (!input) return;
-        input->setString(clipboard::read(), true);
+        if (m_textInput) m_textInput->setString(clipboard::read());
     }
 
     void onClearText(CCObject* sender) {
-        auto input = m_fields->newTextInput;
-        if (!input) return;
-        input->setString("", true);
+        if (m_textInput) m_textInput->setString("");
     }
 
     void openTextMenu(float dt) {
